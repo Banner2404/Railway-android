f5d2ab2fa5d0879aa87ba1f70bdd5ad50bff102a
Eugene Sobol <esobol@insoftgroup.com>
2019-03-23 16:42:04 +0300

    initial commit

---
+++ app/src/main/java/com/esobol/Railway/TicketListActivity.kt
@@ -0,0 +1,12 @@
+package com.esobol.Railway
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TicketListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket_list)
+    }
+}
